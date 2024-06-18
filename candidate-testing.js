e4267058ac6bd2f47d631d7a53a6de1642814861
Harrison Sayre <hsayre42@gmail.com>
2024-06-17 23:20:53 -0400

    <Finished Part 1: Single question quiz>

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,10 +3,15 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
+// DONE. Defined as a simple, empty string.
 let candidateName;
+candidateName = "";
+
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question;
+question = "Who was the first American woman in space? ";
 let correctAnswer;
+correctAnswer = "Sally Ride";
 let candidateAnswer;
 
 
@@ -18,12 +23,21 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
+  //Use console.log to print a prompt, 
+  console.log("Please enter your name: ")
+  //Use realine-sync's input.question() to ge the input then assigns that value to candidateName
+  candidateName = input.question();
 
+  //Print the user's name. For testing purposes.
+  //console.log("Your name is: "+candidateName);
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+  console.log(question);
+  candidateAnswer = input.question();
+  //Print the candidate's answer. For testing purposes.
+  //console.log(candidateAnswer);
 
 }
 
@@ -31,7 +45,12 @@ function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
-
+  //Before comparing get rid of trailing spaces from user's answer. Make both answers upper case to make comparison case insensitive.
+  if(candidateAnswer.trim().toUpperCase() === correctAnswer.toUpperCase()){
+    console.log("Correct!");
+  } else{
+    console.log("Incorrect!");
+  }
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
@@ -46,6 +65,8 @@ function runProgram() {
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
+
+runProgram();
 
 // ----------- Don't write any code or change any code below this line ---------- //
 module.exports = {
